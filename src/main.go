94533741d9fcd4edc66ee4c76160dbaca9ecaee6
unknown <ljovic009@gmail.com>
2025-07-30 02:24:36 +0200

    first commit

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	token := os.Getenv("BOT_TOKEN")
+	_ = token
+}
