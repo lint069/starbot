acdda97afa70cf73f4b10df4ae92fad18d739c2f
lint069 <ljovic009@gmail.com>
2025-08-10 00:50:36 +0200

    feat: improve ping command to show actual response time

--- commands/ping.go
+++ commands/ping.go
@@ -2,11 +2,21 @@ package commands
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func Ping(s *discordgo.Session, m *discordgo.MessageCreate) {
-	latency := s.HeartbeatLatency()
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Pong! `%dms`", latency.Milliseconds()))
+	start := time.Now()
+
+	msg, err := s.ChannelMessageSend(m.ChannelID, "Pong")
+	if err != nil {
+		fmt.Println("failed to send message", err)
+	}
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+
+	s.ChannelMessageEdit(m.ChannelID, msg.ID, fmt.Sprintf("Pong! `%dms`", elapsed))
 }
